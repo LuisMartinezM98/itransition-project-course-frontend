fd133c450e4d8061bf0bb344afca31417f15d6e5
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2024-10-25 01:27:38 -0600

    try to fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage.tsx";
